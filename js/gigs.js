ee315716a2a225e1115f9473a63a67ebc6eeaddf
Vanessa Campos Salvi <vanessacs@Vanessas-MacBook-Air.local>
2017-04-24 12:24:45 +0100

    stripe and about us

--- js/gigs.js
+++ js/gigs.js
@@ -59,8 +59,6 @@ firebase.initializeApp(config);
     // No user is signed in.
     alert("No user signed in");
 }
-
-
 });
 
 
@@ -76,7 +74,7 @@ firebase.initializeApp(config);
 	var costOfTicket = document.getElementById("priceTickets" + z).innerHTML;
 	
 	
-	alert("costOfTicket " + costOfTicket);
+	// alert("costOfTicket " + costOfTicket);
 	
 	
 	LoadPrice(costOfTicket);
@@ -93,20 +91,20 @@ firebase.initializeApp(config);
 		
 		numTicketsBought = document.getElementById("ticketQty").value; 
 		
-		alert("numTicketsBought = " + numTicketsBought);
+		// alert("numTicketsBought = " + numTicketsBought);
 		
-		alert("currentNumTickets = " + currentNumTickets);
-		alert("currentGigId = " + currentGigId);
+		// alert("currentNumTickets = " + currentNumTickets);
+		// alert("currentGigId = " + currentGigId);
 		
 		var user = firebase.auth().currentUser;
 		uid = user.uid;
-		alert("BUY TICKETS uid = " + uid);
+		// alert("BUY TICKETS uid = " + uid);
 		
 		
 		
 		if(parseInt(currentNumTickets) >= parseInt(numTicketsBought))
 		{
-			alert("Tickets are available");	
+			// alert("Tickets are available");	
 			
 			currentNumTickets = currentNumTickets - numTicketsBought;
 			
@@ -156,9 +154,7 @@ firebase.initializeApp(config);
 
 	function UpdateGigInfo2(x,updateThisGig)
 	{
-		
-		
-		
+
 		mydata.child('Gigs').on('value', 
 			function(snapshot) 
 			{
@@ -210,6 +206,64 @@ firebase.initializeApp(config);
 		});
 		
 	});
+
+	// Create a Stripe client
+var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
+// Create an instance of Elements
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '24px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+// Create an instance of the card Element
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server
+      stripeTokenHandler(result.token);
+    }
+  });
+});
 
 
 	
