78fe8ea06b8f116b748e0009274a635c172e4f5e
Vanessa Campos Salvi <vanessacs@Vanessas-MacBook-Air.local>
2017-04-28 11:30:31 +0100

    fixed errors added search js to fan-themed.js

--- js/Fanbase-themed.js
+++ js/Fanbase-themed.js
@@ -1,18 +1,39 @@
 
-$(document).ready(function(){
-	$('.input-daterange').datepicker({
-		format: "dd/mm/yyyy"
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBp7xXz6I76P6scEaSo6fRIyp_HKxrQE2U",
+	authDomain: "fanbase-47b85.firebaseapp.com",
+	databaseURL: "https://fanbase-47b85.firebaseio.com",
+	storageBucket: "fanbase-47b85.appspot.com",
+	messagingSenderId: "1028942516030"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+function doSearch(){
+
+	var search = { 
+		"index": "artists",
+		"type": "artist",
+		//"q": "*"
+	};
+
+	var x = document.getElementById("searchbox").value;
+	search.q=x;
+	console.log(search);
+
+	var key = database.ref("search/request").push(search).key;
+
+	database.ref("search/response").on('value',function(snapshot){ // listen to any search responses 
+
+		console.log(snapshot.val());
+
 	});
 
-	$('.carousel').carousel({
-		interval: 10000
-	});
-
-	$('.search-kw').on("click", function() {
-		console.log('search clicked!')
-	})
-})
-
+}
+	
 function active(){
 	var searchBar=document.getElementById('searchBar');
 	if (searchBar.value =='Search...') {
@@ -28,4 +49,21 @@ function inactive (){
 		searchBar.placeholder=''
 	}
 }
+
+$(document).ready(function(){
+
+	$('.input-daterange').datepicker({
+		format: "dd/mm/yyyy"
+	});
+
+	$('.carousel').carousel({
+		interval: 10000
+	});
+
+	$('.search-kw').on("click", function() {
+		doSearch();
+	})
+
+})
+
 
